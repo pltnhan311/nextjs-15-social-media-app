578176afbe2b0d4a8e925d79ae5447522aa7b17d
pltnhan311 <92429264+pltnhan311@users.noreply.github.com>
2024-10-29 20:43:16 +0700

    add infinite loading new feeds using useInfiniteQuery

--- src/app/api/posts/for-you/route.ts
+++ src/app/api/posts/for-you/route.ts
@@ -1,9 +1,16 @@
 import { validateRequest } from "@/auth";
 import prisma from "@/lib/prisma";
-import { postDataInclude } from "@/lib/types";
+import { postDataInclude, PostsPage } from "@/lib/types";
+import { NextRequest } from "next/server";
 
-export async function GET() {
+export async function GET(req: NextRequest) {
   try {
+    const cursor = req.nextUrl.searchParams.get("cursor") || undefined;
+
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+
+    const pageSize = 3;
+
     const { user } = await validateRequest();
 
     if (!user) {
@@ -13,9 +20,19 @@ export async function GET() {
     const posts = await prisma.post.findMany({
       include: postDataInclude,
       orderBy: { createdAt: "desc" },
+      take: pageSize + 1,
+      cursor: cursor ? { id: cursor } : undefined,
     });
 
-    return Response.json(posts);
+    const nextCursor =
+      posts.length > pageSize ? posts[posts.length - 1].id : null;
+
+    const splitedPosts: PostsPage = {
+      posts: posts.slice(0, pageSize),
+      nextCursor,
+    };
+
+    return Response.json(splitedPosts);
   } catch (error) {
     console.error(error);
     return Response.json({ error: "Internal server error" }, { status: 500 });
